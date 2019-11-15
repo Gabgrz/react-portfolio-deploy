37b74ef08958ed92a337fa95acb6f2558d07f623
Gabriel <gab.120gg@gmail.com>
2019-11-14 20:37:25 -0600

    Updates

---
+++ precache-manifest.0fb03ad5c422a3531922f1a9b9b2f0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a90038c279bcee8e0fbb",
+    "url": "/react-portfolio-deploy/static/js/runtime~main.a90038c2.js"
+  },
+  {
+    "revision": "7aea8db8ebf9c57924e7",
+    "url": "/react-portfolio-deploy/static/js/main.7aea8db8.chunk.js"
+  },
+  {
+    "revision": "df1abf27f99fea3f5486",
+    "url": "/react-portfolio-deploy/static/js/1.df1abf27.chunk.js"
+  },
+  {
+    "revision": "7aea8db8ebf9c57924e7",
+    "url": "/react-portfolio-deploy/static/css/main.e6301766.chunk.css"
+  },
+  {
+    "revision": "a8a00573efb330af95d458602931aedb",
+    "url": "/react-portfolio-deploy/index.html"
+  }
+];
